fc7fa8aa040cefaaf60fe3afb081c855450ad8c3
Kris <kris.choy1@gmail.com>
2019-02-19 17:16:31 +1100

    Merge files

--- MazeGraph.h
+++ MazeGraph.h
@@ -10,9 +10,17 @@
 #define RIGHT 3
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
 
-// graph representation is hidden
-typedef struct Graph *Maze; // a maze is a graph
+typedef struct Graph {
+    Tile** tiles; // matrix of 2D tiles
+    Tile currentTile;
+    int currentTileX;
+    int currentTileY;
+} Graph;
+typedef Graph *Maze; // a maze is a graph
+
 typedef struct Node {
 	short up; // true or false - i.e. a wall or not a wall
 	short left;
@@ -37,5 +45,70 @@ Tile updateCurrentTile(Maze maze, int flagid, int status);
 Tile addPath(Maze maze, int direction);
 // Move current position to an adjacent tile
 Tile move(Maze maze, int direction);
+
+/* Helper functions */
+Tile createTile();
+
+/**
+ * Creates a maze and allocates memory for it
+ * @param  size the maximum square size of the maze
+ * @param  x    x-axis start location of maze
+ * @param  y    y-axis start location of maze
+ * @return      the maze
+ */
+Maze createMaze(int size, int x, int y) {
+	Maze maze = malloc(sizeof(Graph));
+	assert(maze != NULL);
+	maze->tiles = calloc(size * size, sizeof(Tile*));
+	assert(maze->tiles != NULL);
+	// No need to actually make space for tiles - will do when
+	// we find them, to save space
+	maze->tiles[x][y] = createTile();
+	maze->currentTile = maze->tiles[x][y];
+	maze->currentTileX = x;
+	maze->currentTileY = y;
+	return maze;
+}
+
+void printMaze() {
+	// Fill in this function
+}
+
+Tile getCurrentTile(Maze maze) {
+	return maze->currentTile;
+}
+
+Tile updateCurrentTile(Maze maze, int flagid, int status) {
+	Tile t = maze->currentTile;
+	if (t != NULL) {
+		t->flags[flagid] = status;
+	}
+	return t;
+}
+
+Tile addPath(Maze maze, int direction) {
+	// Fill in this function
+	return NULL;
+}
+
+Tile move(Maze maze, int direction) {
+	// Fill in this function
+	return NULL;
+}
+
+// Helper functions
+Tile createTile() {
+	Tile t = malloc(sizeof(Node));
+	assert(t != NULL);
+	t->up = 0;
+	t->left = 0;
+	t->down = 0;
+	t->right = 0;
+	int i;
+	for (i = 0; i < 4; i++) {
+		t->flags[i] = 0;
+	}
+	return t;
+}
 
 #endif
